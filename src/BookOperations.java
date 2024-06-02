25ad2d8ec6b96002ec6ae21fcecd703a732c9a29
Hokusai97 <rw.rafalwiacek@gmail.com>
2024-06-02 16:17:53 +0200

    Zadanie 2

--- src/BookOperations.java
+++
@@ -1,9 +0,0 @@
-package src;
-import java.util.List;
-
-public interface BookOperations {
-    void addBook(Book book);
-    void removeBook(Book book);
-    void updateBook(Book oldBook, Book newBook);
-    List<Book> getBooks();
-}
